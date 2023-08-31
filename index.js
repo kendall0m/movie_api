10ba2a971b6848f805384a8eaeadb93b59ce73ec
kendall0m <marykmiciotto@gmail.com>
2023-08-31 11:26:54 -0400

    created movie list, added GET routes

---
+++ index.js
@@ -0,0 +1,59 @@
+const express = require('express');
+const morgan = require ('morgan');
+const fs = require('fs'); // import built in node modules fs and path 
+const app = express();
+
+const topMovies = [
+    {
+        title: 'The Hunt for Red October',
+        director: 'John McTiernan',
+    },
+    {
+        title: 'The Iron Giant',
+        director: 'Brad Bird',
+    },
+    {
+        title: 'The Philadelphia Story',
+        director: 'George Cukor',
+    },
+    {
+        title: 'The Best Years of Our Lives',
+        director: 'William Wyler',
+    },
+    {
+        title: 'The Caine Mutiny',
+        director: 'Edward Dmytryk',
+    },
+    {
+        title: 'The Godfather',
+        director: 'Francis Ford Coppola',
+    },
+    {
+        title: 'Seabiscuit',
+        director: 'Gary Ross',
+    },
+    {
+        title: 'Pulp Fiction',
+        director: 'Quentin Tarantino',
+    },
+    {
+        title: 'Secretariat',
+        director: 'Randall Wallace',
+    },
+    {
+        title: 'True Grit',
+        director: 'Ethan Coen',
+    }
+]
+// create a write stream (in append mode)
+// a ‘log.txt’ file is created in root directory
+const accessLogStream = fs.createWriteStream(path.join(__dirname, 'log.txt'), {flags: 'a'})
+// setup the logger
+app.use(morgan('combined', {stream: accessLogStream}));
+
+app.get('/movies', (req, res) => {
+  res.json(topMovies);
+});
+app.listen(8080, () => {
+    console.log('Your app is listening on port 8080.');
+  });
