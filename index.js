7fac1442d2ff2d741f107dc8ed2419a5c4ec9abc
kendall0m <marykmiciotto@gmail.com>
2023-12-18 11:55:52 -0500

    Update index.js

--- index.js
+++ index.js
@@ -45,13 +45,13 @@ app.get("/", (req, res) => {
 
 // Get all movies
 app.get('/movies', passport.authenticate('jwt', { session: false }), (req, res) => {
-  Movies.find()
+   Movies.find()
     .then((movies) => {
       res.status(201).json(movies);
     })
-    .catch((error) => {
-      console.error(error);
-      res.status(500).send('Error: ' + error);
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send('Error: ' + err);
     });
 });
 
