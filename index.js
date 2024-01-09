c73225ed0f58d097b7251faf5417e8cc494521c3
kendall0m <marykmiciotto@gmail.com>
2024-01-09 11:31:30 -0500

    cors

--- index.js
+++ index.js
@@ -21,7 +21,21 @@ const bodyParser = require ('body-parser');
 app.use(bodyParser.json());
 
 const cors = require('cors');
-app.use(cors());
+// CORS
+let allowedOrigins = ['http://localhost:8080', 'http://testsite.com', 'http://localhost:1234', 'https://mymovie-api.netlify.app'];
+//allow specific set of origins to access your API
+app.use(cors({
+    origin: (origin, callback) => {
+        if (!origin) return callback(null, true);
+        if (allowedOrigins.indexOf(origin) === -1) {
+            // If a specific origin isn’t found on the list of allowed origins
+            let message = 'The CORS policy for this application doesn’t allow access from origin ' + origin;
+            return callback(new Error(message), false);
+        }
+        return callback(null, true);
+    }
+}));
+
 let auth = require('./auth')(app);
 
 const passport = require('passport');
